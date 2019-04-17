56fd1463d8b7c802badcdff3df00f8b670b692e6
Martin Heinz <martin.heinz1@ibm.com>
2019-04-17 14:05:24 +0200

    Initial commit.

---
+++ backend/src/app/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Creates a router without any middleware by default
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even if you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
+	// Per route middleware, you can add as many as you desire.
+
+	// Listen and serve on 0.0.0.0:8080
+	r.Run(":8080")
+}
---
+++ frontend/startup.sh
@@ -0,0 +1,2 @@
+# Start Nginx with special option in order to run in foreground
+nginx -g "daemon off;"
