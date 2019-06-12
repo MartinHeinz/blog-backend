f22d7983f45683ffa1dc6c38bcba0a8a7ad30b9e
Martin Heinz <martin.heinz1@ibm.com>
2019-06-12 11:47:58 +0200

    Connection to Postgres Database.

--- backend/cmd/src/blog_backend/main.go
+++ backend/cmd/src/blog_backend/main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"net/http"
+)
+
+const (
+	host     = "blog_db"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "postgres"
+	sslmode  = "disable"
 )
 
 func main() {
@@ -22,6 +34,16 @@ func main() {
 		v1.GET("/", testGetData)
 		v1.POST("/", testPostData)
 	}
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	fmt.Println("Successfully connected!")
 
 	r.Run(":1234")
 }
