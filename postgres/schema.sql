ab5bacedd5372916b619b945bf6b9fbe88f31e9e
Martin Heinz <martin.heinz1@ibm.com>
2019-07-10 10:28:22 +0200

    Small updates in schema file.

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -5,6 +5,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -208,11 +209,13 @@ ALTER TABLE ONLY public.tags
 
 -- Retrieval steps:
 --
+-- Remove old volumes
+-- docker volume prune
 -- Add automigrate to Gin start-up
 -- Build container (make container)
 --
 -- docker-compose up
 -- docker exec -it <blog_db_id> /bin/bash
--- pg_dump -s postgres -Upostgres | sed -e '/^--/d'
+-- pg_dump -s blog -Upostgres | sed -e '/^--/d'
 --
 -- Don't forget to remove automigrate
